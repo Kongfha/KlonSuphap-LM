5eabb2b597561c8dfcbf5e78afc566a3824f695b
Kongfha <gong_kampanat@hotmail.co.th>
2023-06-12 01:55:32 +0700

    tren skrip

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
-python3 train.py --mask False \
-                 --train_path Kongfha/KlonSuphap-LM \
-                 --val_path Kongfha/KlonSuphap-LM \
-                 --tokenizer_path ./tokenizer \
-                 --pretrained_path ./model_10_eps \
+!python3 train.py --mask False \
+                 --train_path ./Tagged_Dataset/valid_data_one_tag.json \
+                 --val_path ./Tagged_Dataset/valid_data_one_tag.json \
+                 --tokenizer_path Kongfha/KlonSuphap-LM \
+                 --pretrained_path Kongfha/KlonSuphap-LM \
                  --batch_size 1 \
-                 --epochs 8\
+                 --epochs 1\
                  --save_path ./step_1_model
