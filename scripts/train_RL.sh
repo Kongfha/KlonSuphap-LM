7849b557eb99b4d29b3cefdab767a94812dc02aa
Kongfha <gong_kampanat@hotmail.co.th>
2023-06-13 01:03:28 +0700

    edit add dump

--- scripts/train_RL.sh
+++ scripts/train_RL.sh
@@ -6,3 +6,8 @@ python3 train_RL.py --observation_path ./Tagged_Dataset/observation_list.json \
                     --minibatch_size 250 \
                     --epochs 20 \
                     --save_path ./Model_RL 
+
+python3 dump_RL.py --model Kongfha/KlonSuphap-LM \
+                    --tokenizer Kongfha/KlonSuphap-LM \
+                    --rl ./Model_RL/1500_finish \
+                    --dumpdir ./Model_RL/dumped_model
