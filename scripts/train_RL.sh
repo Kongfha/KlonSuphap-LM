f96d807bbb2b2fd2e0f02dbd50d4f3cde6d624ff
Kongfha <gong_kampanat@hotmail.co.th>
2023-06-13 00:48:07 +0700

    edit RL train script

--- scripts/train_RL.sh
+++ scripts/train_RL.sh
@@ -1,8 +1,8 @@
 python3 train_RL.py --observation_path ./Tagged_Dataset/observation_list.json \
-                    --tokenizer_path  Kongfha/KlonSuphap-LMr \
+                    --tokenizer_path  Kongfha/KlonSuphap-LM \
                     --pretrained_path Kongfha/KlonSuphap-LM \
                     --steps 1500 \
                     --update_interval 250 \
                     --minibatch_size 250 \
                     --epochs 20 \
-                    --save_path ./Model_RL \
+                    --save_path ./Model_RL 
